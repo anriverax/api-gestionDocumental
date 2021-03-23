014400ad6252098c899fb0027cb53da880b149a7
Anibal Rivera <anriverax@outlook.es>
2021-03-23 16:00:03 -0600

    finhis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app";
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4010;
 app.listen(port, "0.0.0.0", () => {
 	console.log(`El servidor esta funcionando en el puerto: ${port}`);
 });
+
